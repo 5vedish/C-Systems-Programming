051a5d9d45de90b8956b9e8ea8e991a6019007b4
CSE320 Student <zhixizhao@cs.stonybrook.edu>
2020-04-19 22:31:48 -0400

    WORKER WORKS

--- hw4/src/worker.c
+++ hw4/src/worker.c
@@ -9,7 +9,6 @@
 
 //My Variables
 
-volatile sig_atomic_t terminated;
 volatile sig_atomic_t canceled;
 
 //Signal Handlers
@@ -18,8 +17,7 @@ void sighup_handler(int sig){
 }
 
 void sigterm_handler(int sig){ //terminate child process via exit
-    canceled = 1;
-    terminated = 1;
+    exit(EXIT_SUCCESS);
 }
 
 
@@ -34,40 +32,45 @@ int worker(void) {
 
     raise(SIGSTOP); //stop itself after initialization
 
-    struct problem *to_read = Malloc(sizeof(struct problem));
-
     while (1){
+    struct problem *to_read = Malloc(sizeof(struct problem)); //make space for problem
 
-    //Read from master process
+    //read from master process
     Read(STDIN_FILENO, to_read, sizeof(struct problem));
-    debug("Reading Problem Header:(%lu, %i, %i, %i, %i)", to_read -> size, to_read -> type, to_read -> id, to_read -> nvars, to_read -> var);
 
-    Realloc(to_read, to_read -> size); //Make space for the follow info
-    //Read the following data
-    ((to_read -> size - sizeof(struct result)) == 0) ? 0 : Read(STDIN_FILENO, to_read -> data, to_read -> size - sizeof(struct problem)); //Don't read twice if size is 0
-    debug("Reading Problem Info: (%s)", to_read -> data);
+    to_read = Realloc(to_read, to_read -> size); //make space for the follow info
+    //read the following data
+    ((to_read -> size - sizeof(struct problem)) == 0) ? 0 : Read(STDIN_FILENO, to_read -> data, to_read -> size - sizeof(struct problem)); //don't read twice if size is 0
 
-    debug("______________________________");
-    //Attempt to solve the problem
-    struct result *to_write = solvers[to_read -> type].solve(to_read, &canceled);
 
-    debug("______________________________");
-    //Write to master process
-    Write(STDOUT_FILENO, to_write, sizeof(struct result));
-
-    ((to_write -> size - sizeof(struct result)) == 0) ? 0 : Write(STDOUT_FILENO, to_write, to_write -> size - sizeof(struct result)); //Don't write twice if size is 0
-
-    raise(SIGSTOP); //Stop itself after sending result
-    
-    if (terminated){ //exit upon sigterm
+    if (to_read -> type == NULL_PROBLEM_TYPE){ //for problem type 0
         exit(EXIT_SUCCESS);
-    }    
-
-    if (canceled){ //if SIGHUP, stop itself *incomplete
-        //might have to write the result stuff here
-        raise(SIGSTOP);
     }
 
+    //attempt to solve the problem
+    struct result *to_write = solvers[to_read -> type].solve(to_read, &canceled);
+
+    if (to_write == NULL){ //if null then send artifical result
+        to_write = Malloc(sizeof(struct result));
+        to_write -> size = sizeof(struct result);
+        to_write -> failed = 1;
+        to_write -> id = to_read -> id;
+    }
+
+    if (canceled){
+        to_write -> failed = 1; //result is allocated but the problem is canceled
+        canceled = 0; //so it could work on multiple problems
+    }
+
+    Free(to_read); //free the problem after things are written
+
+    //write to master process
+    Write(STDOUT_FILENO, to_write, sizeof(struct result));
+
+    ((to_write -> size - sizeof(struct result)) == 0) ? 0 : Write(STDOUT_FILENO, to_write -> data, to_write -> size - sizeof(struct result)); //don't write twice if size is 0
+    Free(to_write); //freeing the result
+
+    raise(SIGSTOP); //stop itself after sending result    
     }
     
     // TO BE IMPLEMENTED
